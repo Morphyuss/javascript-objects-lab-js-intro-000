02dd23f6a9d9f140ef116221a8864007cb6c4b5f
James Stamps Jr <jstampshere@gmail.com>
2020-02-09 00:23:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+ Object.assign({}, object, { [key]: value })
+   return Object.assign({}, object, { [key]: value })
+} 
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+  
+}
+
+
+
