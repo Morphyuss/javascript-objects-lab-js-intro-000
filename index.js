a80d99a1038228aeda991afb7bda6f1a09d447ab
James Stamps Jr <jstampshere@gmail.com>
2020-02-09 00:39:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   Object({}, object, { [key]: value })
-   return 
+   return Object({}, object, { [key]: value })
   
 }
 
