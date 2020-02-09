4469060cdc0eacbd3fc4de271f735d4e89dc1cfc
James Stamps Jr <jstampshere@gmail.com>
2020-02-09 00:42:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   Object.assign({}, object, { [key]: value });
-    
-   return Object.assign({}, object, { [key]: value });
+    const recipe {4 eggs};
+   return Object.assign({}, object, { [key]: value })
   
 }
-
-const recipe {4 eggs}
 
