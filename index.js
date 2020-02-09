0037f334c42496875e3baed90bb72eea43a97808
James Stamps Jr <jstampshere@gmail.com>
2020-02-09 01:34:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ function deleteFromObjectByKey(object, key){
 
 function destructivelyDeleteFromObjectByKey(object, key){
   delete.object[key]
-    return destructivelyDeleteFromObjectByKey
+    return object
 }
